81f7b84762ee36c82233e9e72e011dd00725e0dc
marii¨ <marii@nyu.edu>
2026-05-19 16:57:07 -0400

    Update ingest.mjs [skip ci]

--- scripts/ingest.mjs
+++ scripts/ingest.mjs
@@ -1,18 +1,17 @@
-// change to import 
-const http = await import('https'); // or 'https' for https:// URLs
-const fs = await import('fs');
-// const http = require('http'); // or 'https' for https:// URLs
-// const fs = require('fs');
+const http  = await import('https');
+const fs    = await import('fs');
 
-const filesToDownload = [
-  { url: 'https://ticha-zapotec.github.io/digital-text-explorer-metadata/documents.json', filename: 'documents.json' },
-  { url: 'https://ticha-zapotec.github.io/digital-text-explorer-metadata/archives.json', filename: 'archives.json' },
-  { url: 'https://ticha-zapotec.github.io/digital-text-explorer-metadata/towns.json', filename: 'towns.json' },
-  { url: 'https://ticha-zapotec.github.io/digital-text-explorer-metadata/document_types.json', filename: 'document_types.json' }
+const baseUrl          = 'https://ticha-zapotec.github.io/digital-text-explorer-data'
+const filesToDownload  = [
+  'documents.json', 
+  'archives.json', 
+  'towns.json', 
+  'document_types.json'
 ];
 
-filesToDownload.forEach(({ url, filename }) => {
+filesToDownload.forEach((filename) => {
   const file = fs.createWriteStream(`src/_data/api/${filename}`);
+  const url  = `${baseurl}/${filename}`
   http.get(url, (response) => {
     response.pipe(file);
     file.on('finish', () => {

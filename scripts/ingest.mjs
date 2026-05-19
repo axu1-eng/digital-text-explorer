5598ca29b6a881c2b2a1e5018170369372a5f43a
mnyrop <marii@nyu.edu>
2026-05-19 16:59:45 -0400

    fix typo, mkdir_p [skip ci]

--- scripts/ingest.mjs
+++ scripts/ingest.mjs
@@ -2,6 +2,7 @@ const http  = await import('https');
 const fs    = await import('fs');
 
 const baseUrl          = 'https://ticha-zapotec.github.io/digital-text-explorer-data'
+const targetDir        = 'src/_data/api';
 const filesToDownload  = [
   'documents.json', 
   'archives.json', 
@@ -10,8 +11,16 @@ const filesToDownload  = [
 ];
 
 filesToDownload.forEach((filename) => {
-  const file = fs.createWriteStream(`src/_data/api/${filename}`);
-  const url  = `${baseurl}/${filename}`
+  const file = fs.createWriteStream(`${targetDir }/${filename}`);
+  const url  = `${baseUrl}/${filename}`;
+
+  fs.mkdir(targetDir, { recursive: true }, (err) => {
+    if (err) {
+      console.error(`Error creating directory: ${err.message}`);
+      return;
+    }
+  });
+
   http.get(url, (response) => {
     response.pipe(file);
     file.on('finish', () => {
